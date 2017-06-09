7ebf73a484e7f71225f1fff8501e42c9060d4fac
Kathryn Lybarger <github@zemkat.org>
2017-06-08 20:33:37 -0400

    fix header

--- grima.php
+++ grima.php
@@ -4,6 +4,7 @@
 #
 #       (c) 2017 Kathryn Lybarger. CC-BY-SA
 #
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
