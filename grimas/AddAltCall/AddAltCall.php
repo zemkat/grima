a1198eda19f55b909dcce78d2e2a23c0e5b078f4
Aaron Krebeck <krebeck@wrlc.org>
2018-10-18 15:34:05 -0400

    Create AddAltCall.php

---
+++ grimas/AddAltCall/AddAltCall.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once("../grima-lib.php");
+
+class AddInternalNote extends GrimaTask {
+
+	function do_task() {
+		$item = new Item();
+		$item->loadFromAlmaBarcode($this['barcode']);
+		$item['alternative_call_number'] = $this['note'];
+		$item->updateAlma();
+		if ($this['note'] == "") {
+			$this->addMessage('success',"Alternative Call Number cleared on {$this['barcode']}");
+		} else {
+			$this->addMessage('success',"Alternative Call Number added to {$this['barcode']}");
+		}
+	}
+
+}
+
+AddInternalNote::RunIt();
